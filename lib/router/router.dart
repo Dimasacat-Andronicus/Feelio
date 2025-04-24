f1e45790084b266f057e041152a5b5f51b4fe15f
Dimasacat-Andronicus <dmsctndrncs@gmail.com>
2025-04-24 12:06:40 +0800

    add stats route

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,5 +1,6 @@
-import 'package:feelio/features/mood_list.dart/views/mood_list_layout.dart';
+import 'package:feelio/features/mood_entry/views/mood_home_layout.dart';
 import 'package:feelio/features/mood_list.dart/views/mood_list_page.dart';
+import 'package:feelio/features/mood_stats/views/mood_stats_layout.dart';
 import 'package:feelio/features/mood_view/mood_view_page.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
@@ -25,6 +26,13 @@ final GoRouter _router = GoRouter(
       name: "view-details",
       builder: (BuildContext context, GoRouterState state) {
         return const MoodViewPage();
+      },
+    ),
+    GoRoute(
+      path: '/stats',
+      name: "stats",
+      builder: (BuildContext context, GoRouterState state) {
+        return const MoodStatsPage();
       },
     ),
   ],
